d512f59bc35668dafecdd95d2f33ee4cd615aed1
sudhan247 <56646285+sudhan247@users.noreply.github.com>
2020-04-15 16:17:03 +0530

    Update lca.py

--- lca.py
+++ lca.py
@@ -34,7 +34,7 @@ tin=[0]*(n+1)
 tout=[0]*(n+1)
 eu=[0]*(2*n+1)
 lg=int(log2(n))
-up=[[0 for i in range(n+1)] for j in range(n+1)]
+up=[[0 for i in range(lg+1)] for j in range(n+1)]
 adj=defaultdict(list)
 for _ in range(n-1):
     x,y=map(int,input().split())
